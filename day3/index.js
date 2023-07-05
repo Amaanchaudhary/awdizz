9ecb8aac1fb786062331cd191452914860c78cd7
Amaanchaudhary <chaudharyamaan982@gmail.com>
2023-07-05 23:10:38 +0530

    update

--- day3/index.js
+++ day3/index.js
@@ -1,10 +1,11 @@
 function submitdata(){
-    // alert("Hello Amaan");
-    var name = document.getElementById("xyz").value;
-    console.log(name,"---is Name");
-    // var password = document.getElementById("xyz").value;
-    // if(password < 9){
-    //     return res.send("password should be 8");
-    // }
-    // console.log(password,"---is password");
+    //alert("Hello Amaan");
+    // var name = document.getElementById("xyz").value;
+    // console.log(name,"---is Name");
+    var password = document.getElementById("xyz").value;
+    if(password.length < 9){
+        // return console.log("password should be 8");
+        return  alert("password should be 8");
+    }
+    console.log(password,"---is password");
 }
